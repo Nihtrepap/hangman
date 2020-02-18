c127bb5653a646f3f3b798e2df9cf450811643c7
Philip Abrahamsson <philips.abrahamsson@gmail.com>
2020-02-18 19:23:45 +0100

    MAde alot of things, capture error. Cant continue after win/loss. Need to choose word to start.

--- hangman3/MainPage.xaml.cs
+++ hangman3/MainPage.xaml.cs
@@ -28,37 +28,41 @@ namespace hangman3
         public char[] showLetter;
         public int count;
         public StringBuilder word;
+        public bool playing;
+        public bool chosen;
 
         public MainPage()
         {
             this.InitializeComponent();
-            theWord = "hangman".ToLower();
-            count = theWord.Length;
-            showLetter = new char[count];
-            word = new StringBuilder(count);
-
-
-
-            LoadStringBuild();
-            LoadOpacity();
-        }
-        private void Test_Click(object sender, RoutedEventArgs e)
-        {
-            GetLines();
+            
+            theWord = "X";
+            word = new StringBuilder(theWord.Length);
 
         }
+        private void Test_Click(object sender, RoutedEventArgs e) => GetLines();
+        
 
         private void Reset_Click(object sender, RoutedEventArgs e)
         {
+            playing = false;
             countClick = 0;
+            txtGameOver.Text = "";
+            txtRightGuess.Text = "";
+            txtWrongGuessed.Text = "";
+            word.Clear();
+            //LoadStringBuild();
             LoadOpacity();
+            RemoveFace();
         }
 
         private void Guess_Click(object sender, RoutedEventArgs e)
         {
-            CheckLetter();
-
+            if (txtInputGuess.Text.Length == 1) CheckLetter();
+            else if(playing) txtGameOver.Text = $"Only use ONE char not {txtInputGuess.Text.Length}";
+            txtInputGuess.Text = "";
         }
+
+        
         /// <summary>
         /// This method make the lines dissappear using opacity
         /// </summary>
@@ -78,20 +82,41 @@ namespace hangman3
         public void GetLines()
         {
             countClick++;
-            if (countClick == 1) printBody.Opacity = 1;
-            else if (countClick == 2) printLeftArm.Opacity = 1;
-            else if (countClick == 3) printRightArm.Opacity = 1;
-            else if (countClick == 4) printLeftLeg.Opacity = 1;
-            else if (countClick == 5) { printRightLeg.Opacity = 1; txtGameOver.Text = "YOU JUST KILLED THE STICKMAN\n=("; }
-        }/// <summary>
+            if (countClick == 1) { printHead.Opacity = 1; GetFace(); }
+            else if (countClick == 2) printBody.Opacity = 1;
+            else if (countClick == 3) printLeftArm.Opacity = 1;
+            else if (countClick == 4) printRightArm.Opacity = 1;
+            else if (countClick == 5) printLeftLeg.Opacity = 1;
+            else if (countClick == 6) { printRightLeg.Opacity = 1; theWord = "X"; txtGameOver.Text = "YOU JUST KILLED THE STICKMAN\n=("; playing = false; }
+        }
+        public void GetFace()
+        {
+            eyeFour.Opacity = 1;
+            eyeThree.Opacity = 1;
+            eyeTwo.Opacity = 1;
+            eyeOne.Opacity = 1;
+            mouth.Opacity = 1;
+        }
+        public void RemoveFace()
+        {
+            eyeFour.Opacity = 0;
+            eyeThree.Opacity = 0;
+            eyeTwo.Opacity = 0;
+            eyeOne.Opacity = 0;
+            mouth.Opacity = 0;
+        }
+         /// <summary>
          /// Checks if letter exists in word. If not it will it will call GetLines()
+         /// Will also check if char is guessed already.
          /// 
          /// <see cref="GetLines()"/>
          /// </summary>
         public void CheckLetter()
         {
-            if (theWord.Contains(txtInputGuess.Text.ToLower()))
+            if (theWord.Contains(txtInputGuess.Text.ToLower()) && theWord != "X")
             {
+                playing = true;
+                txtGameOver.Text = "";
                 for (int i = 0; i < theWord.Length; i++)
                 {
                     if (txtInputGuess.Text.ToLower().ToString() == theWord[i].ToString())
@@ -99,26 +124,59 @@ namespace hangman3
                         word.Remove(i, 1);
                         word.Insert(i, txtInputGuess.Text.ToLower());
                     }
-                    else if(word.ToString() == theWord)
+                    if(word.ToString() == theWord)
                     {
                         txtGameOver.Text = "YOU WON THE GAME AND SAVED THE STICKMAN! \n=)";
+                        theWord = "X";
                     }
                     txtRightGuess.Text = word.ToString();
                 }
             }
-            else if (!theWord.Contains(txtInputGuess.Text))
+            else if (!theWord.Contains(txtInputGuess.Text) && theWord != "X")
             {
-                GetLines();
+                if (!txtWrongGuessed.Text.Contains(txtInputGuess.Text)){ GetLines(); AddWrongLetter(); }
+                else txtGameOver.Text = "Already guessed that char";
+            }
+            else
+            {
+                txtGameOver.Text = "Please choose word before starting";
             }
 
         }
-        public void LoadStringBuild()
+        public void AddWrongLetter()
         {
-            for (int i = 0; i < count; i++)
+            txtWrongGuessed.Text += $"{txtInputGuess.Text}, ";
+        }
+        /// <summary>
+        /// This builds up the stringbuild.
+        /// If my stringbuild is not as long as theWord, then input _
+        /// </summary>
+            public void LoadStringBuild()
+        {
+            for (int i = 0; i < theWord.Length; i++)
             {
-                if (word.ToString().Length != count)
+                if (word.ToString().Length != theWord.Length)
                     word.Insert(i, '_');
             }
+        }
+        /// <summary>
+        /// This will only work if the word is not chosen. 
+        /// </summary>
+        public void NewGuessWord()
+        {
+            if (playing == false)
+            {
+                theWord = txtNewWord.Text.ToLower();
+                playing = true; 
+                LoadStringBuild();
+                txtNewWord.Text = "";
+            }
+            else txtGameOver.Text = "Cant change word while in game";
+        }
+
+        private void NewWord_Click(object sender, RoutedEventArgs e)
+        {
+            NewGuessWord();
         }
     }
 }
